04ffc36cec18d91d83880a9f982fa30665311787
Wayne Knoesen <wayneknoesen@Waynes-MacBook-Pro.local>
2014-09-24 20:51:52 +0200

    Initial Commit

---
+++ Stormy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Wayne Knoesen on 24/09/14.
+//  Copyright (c) 2014 LeetApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
