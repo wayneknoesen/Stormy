ac5277cc14dad54a8d7c4606c1d281b8bb6a0887
Wayne Knoesen <wayneknoesen@Waynes-MacBook-Pro.local>
2014-10-02 17:28:20 +0200

    Stromy PUSH IT LIKSOM

--- Stormy/Current.swift
+++ Stormy/Current.swift
@@ -7,3 +7,103 @@
 //
 
 import Foundation
+import UIKit
+
+
+struct Current {
+    
+    var currentTime: String?
+    var temperature: Int
+    var humidity: Double
+    var precipProbability: Double
+    var summary: String
+    var icon: UIImage?
+    
+    init(weatherDictionary: NSDictionary) {
+        let currentWeatherDictionary: NSDictionary = weatherDictionary["currently"] as NSDictionary
+        
+        temperature = currentWeatherDictionary["temperature"] as Int
+        humidity = currentWeatherDictionary["humidity"] as Double
+        precipProbability = currentWeatherDictionary["precipProbability"] as Double
+        summary = currentWeatherDictionary["summary"] as String
+        
+        let currentTimeIntValue = currentWeatherDictionary["time"] as Int
+        currentTime = dateStringFromUnixtime(currentTimeIntValue)
+        
+        let iconString = currentWeatherDictionary["icon"] as String
+        icon = weatherIconFromString(iconString)
+        
+    }
+    
+    func dateStringFromUnixtime(unixTime: Int) -> String {
+        let timeinSeconds = NSTimeInterval(unixTime)
+        let weatherDate = NSDate(timeIntervalSince1970: timeinSeconds)
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.timeStyle = .ShortStyle
+        
+        return dateFormatter.stringFromDate(weatherDate)
+        
+    }
+    
+    func weatherIconFromString(stringIcon: String) ->UIImage {
+        var imageName: String
+        
+        switch stringIcon {
+            case "clear-day":
+                imageName = "clear-day"
+            case "clear-night":
+                imageName = "clear-night"
+            case "rain":
+                imageName = "rain"
+            case "snow":
+                imageName = "snow"
+            case "sleet":
+                imageName = "sleet"
+            case "wind":
+                imageName = "wind"
+            case "fog":
+                imageName = "fog"
+            case "cloudy":
+                imageName = "cloudy"
+            case "partly-cloudy-day":
+                imageName = "partly-cloudy-day"
+            case "partly-cloudy-night":
+                imageName = "partly-cloudy-night"
+            default:
+                imageName = "default"
+        }
+        
+        var iconImage = UIImage(named: imageName)
+        return iconImage
+        
+    }
+    
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+}
+
+
+
+
+
+
--- Stormy/Stromy play here.playground/section-1.swift
+++ Stormy/Stromy play here.playground/section-1.swift
@@ -2,4 +2,21 @@
 
 import UIKit
 
+
+
+var languages = ["iOS" : "Swift", "Android" : "Java"]
+var iosLanguage = ["iOS"]
+
+
+
 var str = "Hello, playground"
+
+
+let courseID = 25
+let treehouseBaseURL = NSURL (string: "https://api.teamtreehouse.com/course/")
+let courseURL = NSURL (string: "\(courseID)", relativeToURL: treehouseBaseURL)
+
+let apiKey = "7c505f66e89315ea30ef5096d540f444"
+
+let baseURL = NSURL (string: "https://api.forecast.io/forecast/\(apiKey)/")
+let forcastURL = NSURL (string: "37.8267,-122.423", relativeToURL: baseURL)
